3ab28b64fbc5444596dfaf7fcb74c4d4f45d9e0f
Jewel <jeweljoesphprince@gmail.com>
2021-05-03 13:53:24 +0530

    Slot search of a  day along with parser completed

---
+++ parser.py
@@ -0,0 +1,15 @@
+# Functions used to parse the api responses
+
+def find_by(response, age_limit):
+    """
+    Function to parse the response to a dict mapping each centre to available slots
+    :param response: API response
+    :param age_limit: Desired age limit
+    :return: dictionary with centres mapped against available slots
+    """
+    available = list(filter(lambda each: each.get("available_capacity") > 0 and each.get("min_age_limit") <= age_limit,
+                            response))
+    available_dict = {}
+    for each in available:
+        available_dict[each["name"]] = available_dict.get(each["name"], 0)+each["available_capacity"]
+    return available_dict
--- pincode.py
+++ pincode.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from requests import get
 from const import API_WEEK_PIN, API_DAY_PIN
+from parser import find_by
 
 
 class PinCode:
@@ -9,27 +10,35 @@ class PinCode:
     def __get_api_call(api, pin, search_date):
         """
         Function to perform the api get call
-        :param api:
-        :param pin:
-        :param search_date:
+        :param api: API for performing the get call
+        :param pin: PinCode of the location
+        :param search_date: date from which slots are to be searched
         :return: Get call response
         """
         search_date = search_date if search_date else datetime.today().strftime('%d-%m-%Y')
         get_call = api + "?pincode=" + str(pin) + "&date=" + str(search_date)
-        response = get(get_call)
-        return response
+        try:
+            response = get(get_call)
+            response = response.json() if response.status_code == 200 else None
+        except Exception as e:
+            print("Error while API call!! Please check your internet connection and try again after some time...")
+            response = None
+        finally:
+            return response
 
     def day(self, pin, search_date):
         """
         Function to get slots available on a given date
 
-        :param pin:
-        :param search_date:
-        :return:
+        :param pin: Pin Code of the location
+        :param search_date: date from which slots are to be searched
+        :return: Dictionary with centres mapped to available slots
         """
         search_date = search_date if search_date else datetime.today().strftime('%d-%m-%Y')
         response = self.__get_api_call(api=API_DAY_PIN, pin=pin, search_date=search_date)
-        print(response)
+        if response:
+            response = find_by(response=response.get("sessions", []), age_limit=45)
+        return response
 
     def week(self, pin, search_date):
         """
