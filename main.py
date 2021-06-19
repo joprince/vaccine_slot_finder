a41f291d31a36ae985c5381bc5148850d775715e
Jewel <jeweljoesphprince@gmail.com>
2021-06-19 17:47:02 +0530

    Script to run process in a set interval done

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+from multiprocessing import Queue
+from multiprocessing.context import Process
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
 from pincode import PinCode
+from multithreader import process_manager
+import sys
 
 
 def main():
     pin_slot = PinCode()
-    pin_slot.week(pin=744302, search_date="03-05-2021")
+    q = Queue()
+    # Process(target=process_manager, args=(pin_slot.day, q, 686575, "11-05-2021", 18)).start()
+    Process(target=process_manager, args=(pin_slot.week, q, 744302, "11-05-2021", 45)).start()
+    while True:
+        response = q.get()
+        print(response)
+
+    # Process(target=process_manager, args=(pin_slot.day, 744302, "03-05-2021")).start()
+    # pin_slot.week(pin=744302, search_date="03-05-2021")
     # print(pin_slot.day(pin=744302, search_date="03-05-2021"))
 
 
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("Vaccine slot finder")
+
+    win.show()
+    sys.exit(app.exec_())
+
+
 if __name__ == "__main__":
-    main()
+    # main()
+    window()
--- pincode.py
+++ pincode.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from requests import get
+
 from const import API_WEEK_PIN, API_DAY_PIN
 from parser import find_by, calender
 
@@ -17,19 +18,25 @@ class PinCode:
         """
         search_date = search_date if search_date else datetime.today().strftime('%d-%m-%Y')
         get_call = api + "?pincode=" + str(pin) + "&date=" + str(search_date)
+        headers = {"accept": "application/json",
+                   "Accept-Language": "hi_IN",
+                   "Referer": "https://apisetu.gov.in/public/marketplace/api/cowin/cowinapi-v2",
+                   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                 "Chrome/90.0.4430.93 Safari/537.36"}
         try:
-            response = get(get_call)
-            response = response.json() if response.status_code == 200 else None
+            response = get(get_call, headers=headers)
+            response = response.json() if response.status_code == 200 else {}
         except Exception as e:
             print("Error while API call!! Please check your internet connection and try again after some time...")
-            response = None
+            response = {}
         finally:
             return response
 
-    def day(self, pin, search_date):
+    def day(self, pin, search_date, age):
         """
         Function to get slots available on a given date
 
+        :param age: age of the user
         :param pin: Pin Code of the location
         :param search_date: date from which slots are to be searched
         :return: Dictionary with centres mapped to available slots
@@ -37,17 +44,18 @@ class PinCode:
         search_date = search_date if search_date else datetime.today().strftime('%d-%m-%Y')
         response = self.__get_api_call(api=API_DAY_PIN, pin=pin, search_date=search_date)
         if response:
-            response = find_by(response=response.get("sessions", []), age_limit=45)
+            response = find_by(response=response.get("sessions", []), age_limit=age)
         return response
 
-    def week(self, pin, search_date):
+    def week(self, pin, search_date, age):
         """
         Function to get slots available for 7 days from a given date
-        :param pin:
-        :param search_date:
-        :return:
+        :param age: age of the user
+        :param pin: Pin Code of the location
+        :param search_date: date from which slots are to be searched
+        :return: Dictionary with centres mapped to available slots
         """
         search_date = search_date if search_date else datetime.today().strftime('%d-%m-%Y')
         response = self.__get_api_call(api=API_WEEK_PIN, pin=pin, search_date=search_date)
-        response = calender(response=response.get("centers", []), age_limit=45)
-        print(response)
+        response = calender(response=response.get("centers", []), age_limit=age)
+        return response
